c2e38bd182ac1d313dec2bb3c1cec61d12ce9e0c
Alejandro Bay <alejandrobay23@gmail.com>
2024-10-05 00:21:34 -0400

    Movement Script

--- Assets/Scripts/Player/ContollerManager.cs
+++ Assets/Scripts/Player/ContollerManager.cs
@@ -9,9 +9,36 @@ public class ControllerManager : MonoBehaviour
 
     private PlayerController _playerController;
     private Rigidbody2D _rb;
+    private Vector2 _movement;
 
     private void Awake()    
     {
-        playerController = new PlayerController();    
+        _playerController = new PlayerController();    
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _playerController.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerActions();
+    }
+
+    private void PlayerActions()
+    {
+        _movement = _playerController.Movement.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        _rb.MovePosition( _rb.position + _movement * _speed * Time.fixedDeltaTime);
     }
 }
