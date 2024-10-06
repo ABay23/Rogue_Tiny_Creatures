7b9f7cf3769c437aac61d00d89199b7c74129d04
Alejandro Bay <alejandrobay23@gmail.com>
2024-10-06 00:54:31 -0400

    update on Enemy Ailments

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -4,7 +4,14 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
-{
+{   
+    public enum EnemyType { Poison, Fire, Ice, Normal }
+
+    [SerializeField] private EnemyType _enemyType;
+    [SerializeField] private GameObject _poisonAilment;
+    [SerializeField] private GameObject _fireAilment;
+    [SerializeField] private GameObject _iceAilment;
+
     [SerializeField] private int _startingHealth = 10;
     [SerializeField] private float _knockBackForce = 15f;
 
@@ -30,7 +37,27 @@ public class EnemyHealth : MonoBehaviour
         }
     }
 
-    private void Die() {
+    private void Die() 
+    {
+        SpawnAilment();
         Destroy(gameObject);
+    }
+
+    private void SpawnAilment()
+    {
+        switch (_enemyType)
+        {
+            case EnemyType.Poison:
+                Instantiate(_poisonAilment, transform.position, Quaternion.identity);
+                break;
+            case EnemyType.Fire:
+                Instantiate(_fireAilment, transform.position, Quaternion.identity);
+                break;
+            case EnemyType.Ice:
+                Instantiate(_iceAilment, transform.position, Quaternion.identity);
+                break;
+            // case EnemyType.Normal:
+            //     break;
+        }
     }
 }
